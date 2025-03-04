d2b26237b8c187150baf0d24f6ee916eaa2b1891
dlqjsdp <dlqjsdp224@gmail.com>
2025-03-04 18:01:56 +0900

    20250304(2)

--- Java20250304_반복문/src/ch04/ex01/ForExam04_배열07.java
+++ Java20250304_반복문/src/ch04/ex01/ForExam04_배열07.java
@@ -6,33 +6,14 @@ public class ForExam04_배열07 {
 
 	public static void main(String[] args) {		
 
-		for(int i=0; i<5; i++) {
-			for(int j=0; j<5; j++) {
-				System.out.print("* ");
+		//구구단 출력
+		
+		for(int i=2; i<=9; i++) {
+			System.out.printf("*******%d단*******\n", i);
+			for(int j=1; j<=9; j++) {
+				System.out.printf("%dx%d=%d\n", i, j, i*j);
 			}
 			System.out.println();
 		}
-		
-		System.out.println();
-		System.out.println();
-		
-		// 이중 for문
-		
-		for(int i=0; i<5; i++){
-			for(int j=0; j<i+1; j++)  {
-				System.out.print("* ");
-			}
-			System.out.println();	
-		}
-		System.out.println();	
-		System.out.println();	
-		
-		for(int i=0; i<5; i++){
-			for(int j=0; j<5-i; j++)  {
-				System.out.print("* ");
-			}
-			System.out.println();	
-		}
-
 	}
 }
---
+++ Java20250304_반복문/src/ch04/ex01/WhileExam03.java
@@ -0,0 +1,37 @@
+package ch04.ex01;
+
+import java.util.Scanner;
+
+public class WhileExam03 {
+
+	public static void main(String[] args) {
+
+		/*
+		 * Scanner sc = new Scanner(System.in);
+			int sum = 0;
+			System.out.println("정수입력 >>");
+			int number = sc.nextInt();
+		
+			for(int i=1; i<=number; i++) {
+				if (i % 2 == 1) {       // 홀수인가요?
+				sum += i;           // 홀수면 누적
+			}
+			System.out.println(sum);
+		 */
+		
+		Scanner sc = new Scanner(System.in);
+		int sum = 0;
+		System.out.println("정수입력 >>");
+		int number = sc.nextInt();
+		int i=1;
+		
+		while(i<=number) {
+			if(i%2 == 1)  //1부터 number까지 홀수합만 누적
+				sum += i;
+			i++;			
+		}
+		System.out.println(sum);
+	
+	}
+
+}
